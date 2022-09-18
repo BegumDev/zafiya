eab5d09ac3bbbb898b26d8e1583a4b5155c2231a
BegumDev <ruk90@outlook.com>
2022-09-18 17:16:14 +0000

    Add home app and templates.

--- zafiya/settings.py
+++ zafiya/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',  # used for logging in and out
     'allauth.socialaccount',  # used for logging via social accounts
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'zafiya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
