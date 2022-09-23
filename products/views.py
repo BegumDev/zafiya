3fd362f5f5ae6864457b4b5a802d7796f5d7dca2
BegumDev <ruk90@outlook.com>
2022-09-23 12:01:36 +0000

    Add product details page.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
-# Create your views here.
+
 def all_products(request):
     """ A view to show all products """
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
     
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ A view to show product details """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/product_details.html', context)
