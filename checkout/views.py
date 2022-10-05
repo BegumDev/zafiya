7b7addfa5ebe035070f426de73073e1a9fad9ed7
BegumDev <ruk90@outlook.com>
2022-10-05 19:46:36 +0000

    Add stripe payments card js.

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KXp07HzSDZpmx6DtJTZjjqIQxiZfhX7zAAGRhUf1KXUQ39cgRc8AqZmsZzBpIV63g5hY1rbKqomiafppywxc3Pq00N8VACZsg',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
