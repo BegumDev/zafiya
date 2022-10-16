556d9277ce208ddcfc4c10c7dd2a3ad5af30d966
BegumDev <ruk90@outlook.com>
2022-10-16 20:23:33 +0000

    Add profiles forms and fix bug where labels are showing when it is supposed to just show placeholders.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     """ to view user profile """
     
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+    
+    form = UserProfileForm(instance=profile)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
+
