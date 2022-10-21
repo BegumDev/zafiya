47bc47fbd227c8d64044db83ddcd75a8b10c1883
BegumDev <ruk90@outlook.com>
2022-10-21 12:56:49 +0000

    Add admin functionality to add products, its form, views and template.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Product
+from .forms import ProductForm
 from django.contrib import messages
 
 
@@ -25,3 +26,16 @@ def product_details(request, product_id):
     }
     
     return render(request, 'products/product_details.html', context)
+
+
+def add_product(request):
+    """ A view for admin to add products """
+
+    form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
