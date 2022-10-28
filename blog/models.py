e90fad2a3ade5228ebbff902be6fb0ad218ed922
BegumDev <ruk90@outlook.com>
2022-10-28 22:36:28 +0000

    User 1 was from previous migration aand cannot be found now. Comments are not attaching to the post.

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     blog_title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=5)
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
